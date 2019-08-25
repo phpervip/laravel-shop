d4e9b50ea6a68240edbc5777a68108e17c1d934d
phper <2998658517@qq.com>
2019-08-25 13:01:54 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::group(['middleware'=>'auth'],function(){
 });
 
 Route::group(['middleware'=>['auth','verified']],function(){
-		Route::any('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+		Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
 		Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
 		Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
 		Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');

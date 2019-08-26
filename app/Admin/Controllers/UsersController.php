ad679be2b1ad1da5a09e9f4960e285d63af5faf3
phper <2998658517@qq.com>
2019-08-26 07:45:04 +0000

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+use Encore\Admin\Layout\Content;
+
+class UsersController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Models\User';
+
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用户列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('email', '邮箱');
+        $grid->column('email_verified_at', '已验证邮箱')->display(function($value){
+            return $value?'是':'否';
+        });
+        $grid->column('created_at', '注册时间');
+
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions) {
+            // 不在每一行后面展示查看按钮
+            $actions->disableView();
+            // 不在每一行后面展示删除按钮
+            $actions->disableDelete();
+            // 不在每一行后面展示编辑按钮
+            $actions->disableEdit();
+        });
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('email', __('Email'));
+        $show->field('email_verified_at', __('Email verified at'));
+        $show->field('password', __('Password'));
+        $show->field('remember_token', __('Remember token'));
+        $show->field('email_verified', __('Email verified'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User);
+
+        $form->text('name', __('Name'));
+        $form->email('email', __('Email'));
+        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+        $form->password('password', __('Password'));
+        $form->text('remember_token', __('Remember token'));
+        $form->switch('email_verified', __('Email verified'));
+
+        return $form;
+    }
+}

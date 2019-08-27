651061bc8737898f1a457ad73d1d27c6b43f4602
phper <2998658517@qq.com>
2019-08-27 07:30:47 +0000

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,7 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {
@@ -60,8 +61,10 @@ class OrdersController extends Controller
             $skuIds = collect($items)->pluck('sku_id');
             $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
 
+            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
             return $order;
         });
+
 
         return $order;
     }

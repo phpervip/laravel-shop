023d0b139edce7aa6338363e6305459c85a30ae6
phper <2998658517@qq.com>
2019-09-17 07:55:33 +0000

    管理后台 - 同意退款 - 微信支付退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
 
-            // $config['notify_url'] = route('payment.alipay.notify');
-            $config['notify_url'] = 'http://requestbin.net/r/1nsc8ik1';
+            $config['notify_url'] = route('payment.alipay.notify');
+            //$config['notify_url'] = 'http://requestbin.net/r/1nsc8ik1';
             $config['return_url'] = route('payment.alipay.return');
 
             // 判断当前项目运行环境是否为线上环境
@@ -41,7 +41,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
             // $config['notify_url'] = 'http://requestbin.fullcontact.com/[替换成你自己的url]';
-            $config['notify_url'] = 'http://requestbin.net/r/15g55b81';
+            // $config['notify_url'] = 'http://requestbin.net/r/1nsc8ik1';
+            $config['notify_url'] = route('payment.wechat.notify');
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {

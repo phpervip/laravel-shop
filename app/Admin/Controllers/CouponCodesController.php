16365e156ff787f7bcb28ad41b6fa35311d670c4
phper <2998658517@qq.com>
2019-09-17 15:07:56 +0000

    管理后台 -新增、修改、删除优惠券

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -64,6 +64,13 @@ class CouponCodesController extends AdminController
 
     }
 
+    public function create(Content $content)
+    {
+         return $content
+            ->header('新增优惠券')
+            ->body($this->form());
+    }
+
     /**
      * Make a show builder.
      *
@@ -100,17 +107,44 @@ class CouponCodesController extends AdminController
     {
         $form = new Form(new CouponCode);
 
-        $form->text('name', __('Name'));
-        $form->text('code', __('Code'));
-        $form->text('type', __('Type'));
-        $form->decimal('value', __('Value'));
-        $form->number('total', __('Total'));
-        $form->number('used', __('Used'));
-        $form->decimal('min_amount', __('Min amount'));
-        $form->datetime('not_before', __('Not before'))->default(date('Y-m-d H:i:s'));
-        $form->datetime('not_after', __('Not after'))->default(date('Y-m-d H:i:s'));
-        $form->switch('enable', __('Enable'));
+        $form->display('id', 'ID');
+        $form->text('name', '名称')->rules('required');
+        $form->text('code', '优惠码')->rules(function(Form $form){
+            if( $id=$form->model()->id ){
+                return 'nullable|unique:coupon_codes,code,'.$id.',id';
+            }else{
+                return 'nullable|unique:coupon_codes';
+            }
+        });
+
+
+        $form->radio('type','类型')->options(CouponCode::$typeMap)->rules('required');
+        $form->text('value', '折扣')->rules(function($form){
+            if(request()->input('type')===CouponCode::TYPE_PERCENT){
+                return 'required|numeric|between:1,99';
+            }else{
+                return 'required|numeric|min:0.01';
+            }
+        });
+        $form->text('total', '总量')->rules('required|numeric|min:0');
+        $form->text('min_amount', '最低金额')->rules('required|numeric|min:0');
+        $form->datetime('not_before', '开始时间');
+        $form->datetime('not_after', '结束时间');
+        $form->radio('enabled','启用')->options(['1'=>'是','0'=>'否']);
+
+        $form->saving(function(Form $form){
+            if(!$form->code){
+                $form->code = CouponCode::findAvailableCode();
+            }
+        });
 
         return $form;
+    }
+
+
+
+    public function edit($id,Content $content)
+    {
+        return $content->header('编辑优惠券')->body($this->form()->edit($id));
     }
 }

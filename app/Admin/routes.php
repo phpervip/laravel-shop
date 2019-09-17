20aecd5069cfa41959aaacce9cb6e78bcec1bd46
phper <2998658517@qq.com>
2019-09-17 09:43:08 +0000

    管理后台 -优惠券列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -42,4 +42,6 @@ Route::group([
 
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
 
+    $router->get('coupon_codes','CouponCodesController@index');
+
 });
